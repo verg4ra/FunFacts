af09e63518bdb55e8987ad2588642e75e57e42be
Edwin Vergara <heydot.at@gmail.com>
2016-04-25 16:07:30 -0500

    Creating a Data Collection

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    let facts = ["Ants stretch when they wake up in the morning", "Ostriches can run faster than horses"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        funFactLabel.text = "An interesting fact!"
+        
+        funFactLabel.text = facts[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +25,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
-        funFactLabel.text = "Another interesting fact!"
+        funFactLabel.text = facts[1]
     }
 
 }
