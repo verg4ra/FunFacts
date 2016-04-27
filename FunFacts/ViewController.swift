52825ef9887be1fdf1c022dd5fd261b3d73103fc
Edwin Vergara <heydot.at@gmail.com>
2016-04-26 19:40:02 -0500

    Adding a Pop of Color

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
+        let newColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+        view.backgroundColor = newColor
         funFactLabel.text = factModel.getRandomFact()
     }
 
